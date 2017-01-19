58bcbef50600894d6d2d48a555ad451579d50396
Stephen Done <sdone@simplifile.com>
2017-01-19 16:53:17 -0700

    enforcing null checks with Either

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const Box = x =>
         fold: f => f(x)
     })
 
+const Right = x =>
+({
+    map: f => Right(f(x)),
+    fold: (f, g) => g(x)
+})
+
+const Left = x =>
+({
+    map: f => Left(x),
+    fold: (f, g) => f(x)
+})
+
+const fromNullable = x =>
+    x != null ? Right(x) : Left(null)
+
 const nextCharForNumberString = str =>
     Box(str)
     .map(s => s.trim())
@@ -34,3 +49,12 @@ const applyDiscount = (price, discount) =>
 const result2 = applyDiscount('$5.00', '20%');
 
 console.log(result2);
+
+const findColor = name => fromNullable({red: '#ff4444', blue: '#3b5998', yellow: '#fff68f'}[name]);
+
+const res = findColor('bluegreen')
+            .map(c => c.slice(1))
+            .map(c => c.toUpperCase())
+            .fold(e => 'no color', x => x)
+
+console.log(res);
